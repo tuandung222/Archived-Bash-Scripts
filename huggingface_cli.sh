1a0972a4a33483da030a7fa854e96dc3fce566eb
Dung Vo Pham Tuan <75377334+tuandung222@users.noreply.github.com>
2025-03-19 11:47:05 +0700

    Update and rename huggingface_cli.sh to upload_by_huggingface_cli.sh

--- huggingface_cli.sh
+++
@@ -1,4 +0,0 @@
-# Upload file
-
-# huggingface-cli upload <your-username>/<repo-name> <local-folder-path> <destination-path-in-repo> --repo-type <type>
-huggingface-cli upload tuandunghcmut/vscode_dev vscode_dev.zip --repo-type model
---
+++ upload_by_huggingface_cli.sh
@@ -0,0 +1,13 @@
+# Upload file
+
+# Template 1
+# huggingface-cli upload \
+# tuandunghcmut/TDung_Legacy_0 \
+# /dscilab_dungvo/workspace/TDung \
+# --repo-type model \
+# --private
+
+
+# Template 2
+# huggingface-cli upload <your-username>/<repo-name> <local-folder-path> <destination-path-in-repo> --repo-type <type>
+# huggingface-cli upload tuandunghcmut/vscode_dev vscode_dev.zip --repo-type model
